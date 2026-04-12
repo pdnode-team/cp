c50b745b578da3c14600b9c6c82717453c0bf89c
BretRen <bretren2013@gmail.com>
2026-04-12 15:13:42 -0700

    fix: 404 error & update build.yml
    Update
    - Add version on filename
    - Add prerelease check
    - Fix main.go (pocketbase) 404 error
    - Delete debug code in main.go

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"embed"
-	"fmt"
 	"io/fs"
 	"log"
-	"os"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
@@ -18,19 +16,6 @@ import (
 var embeddedFiles embed.FS
 
 func main() {
-	fmt.Println("--- 开始检查嵌入的文件列表 ---")
-	err := fs.WalkDir(embeddedFiles, ".", func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		fmt.Printf("发现路径: %s (目录: %v)\n", path, d.IsDir())
-		return nil
-	})
-	if err != nil {
-		fmt.Println("检查失败:", err)
-	}
-	fmt.Println("--- 检查结束 ---")
-
 	app := pocketbase.New()
 
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
@@ -40,8 +25,12 @@ func main() {
 	})
 
 	app.OnServe().BindFunc(func(se *core.ServeEvent) error {
+		publicFS, err := fs.Sub(embeddedFiles, "pb_public")
+		if err != nil {
+			return err
+		}
 		// serves static files from the provided public dir (if exists)
-		se.Router.GET("/{path...}", apis.Static(os.DirFS("./pb_public"), false))
+		se.Router.GET("/{path...}", apis.Static(publicFS, true))
 
 		return se.Next()
 	})
