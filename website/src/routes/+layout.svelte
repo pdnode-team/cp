48cea012beeb1a2a55a68151ea2a005ace60d2df
BretRen <bretren2013@gmail.com>
2026-04-18 21:35:59 -0700

    Update to v0.3.0

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 >
 	<div class="flex-1">
 		<a href="/" class="text-xl font-bold tracking-tight transition-colors hover:text-primary">
-			Pdnode CP Website <div class="badge badge-soft">v0.2.13</div>
+			Pdnode CP Website <div class="badge badge-soft">v0.3.0</div>
 		</a>
 	</div>
 	<div class="flex-none gap-4">
