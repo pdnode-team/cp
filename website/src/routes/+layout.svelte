52c8d48f60e3d9d9a2066afbb278028823ed998f
BretRen <bretren2013@gmail.com>
2026-04-27 20:22:27 -0700

    v0.5.1

--- website/src/routes/+layout.svelte
+++ website/src/routes/+layout.svelte
@@ -31,7 +31,7 @@
 >
 	<div class="flex-1">
 		<a href="/" class="text-xl font-bold tracking-tight transition-colors hover:text-primary">
-			Pdnode CP Website <div class="badge badge-soft">v0.5.0</div>
+			Pdnode CP Website <div class="badge badge-soft">v0.5.1</div>
 		</a>
 	</div>
 	<div class="flex-none gap-4">
